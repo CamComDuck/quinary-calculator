112e2c53287f34e306f7086aabd47f9375c8ac5a
Camden Hovell <camdenhovell@gmail.com>
2025-08-29 18:35:09 -0400

    Add buttons to UI

--- project/testing/test_calculator.gd
+++ project/testing/test_calculator.gd
@@ -3,6 +3,6 @@ extends GutTest
 
 
 func test_divide_pass():
-	var calculator_script = load("res://ui/calculator.gd").new()
+	var calculator_script = load("res://calculator/calculator.gd").new()
 	var division_result : int = calculator_script.divide(4, 2)
 	assert_eq(division_result, 2, "4 / 2 = 2")
