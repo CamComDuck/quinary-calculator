6b183ab7eaf7b49f36e3ffb5234e544792bacd06
Camden Hovell <camdenhovell@gmail.com>
2025-08-29 19:10:11 -0400

    Connect buttons to script with signals

--- project/calculator/calculator.gd
+++ project/calculator/calculator.gd
@@ -3,11 +3,16 @@ extends Node2D
 
 var last_button_pressed : int = -1
 
+@onready var display_text := %DisplayText as DisplayText
+
 
 func divide(num_1 : int, num_2 : int) -> float:
 	return num_1 / num_2
+	
+	
+func _update_display(new_display_text : String) -> void:
+	display_text.update_display(new_display_text)
 
 
-func _on_button_1_pressed() -> void:
-	last_button_pressed = 1
-	print("1")
+func _on_button_pressed(button_pressed : ButtonsGrid.calc_button) -> void:
+	print(ButtonsGrid.calc_button.keys()[button_pressed])
