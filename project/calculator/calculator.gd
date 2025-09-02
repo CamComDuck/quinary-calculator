2bb85b354883997c52b7344d3c385484d76d03a8
Gabe-Manning <gabemanning10@gmail.com>
2025-09-02 16:11:15 -0400

    Fixed division function

--- project/calculator/calculator.gd
+++ project/calculator/calculator.gd
@@ -8,8 +8,6 @@ var last_button_pressed : int = -1
 func divide_calc(num_1 : int, num_2 : int) -> float:
 	if num_2 == 0:
 		return -100
-	elif (num_1 < 0 or num_2 < 0 or num_1 > 4 or num_2 > 4):
-		return -100
 	else:
 		return num_1 / num_2
 
--- project/testing/test_calculator.gd
+++ project/testing/test_calculator.gd
@@ -12,12 +12,6 @@ func test_divide_calc_pass2():
 	var division_result : int = calculator_script.divide_calc(4, 0)
 	assert_eq(division_result, -100, "4 / 0 = undefined, which is shown as -100 in calculation")
 
-func test_divide_calc_pass3():
-	var calculator_script = load("res://calculator/calculator.gd").new()
-	var division_result : int = calculator_script.divide_calc(5, -1)
-	assert_eq(division_result, -100, "5 / -1 = undefined, which is shown as -100. (Numbers above 4 or 
-		below 0 not allowed), shown as -100 in calculation)")
-
 func test_divide_out_pass1():
 	var calculator_script = load("res://calculator/calculator.gd").new()
 	var output : String = calculator_script.divide_out(-100)
