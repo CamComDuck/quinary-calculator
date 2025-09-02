ea3dafae1cc4207011a6ccae8cc320009a9b5e82
Gabe-Manning <gabemanning10@gmail.com>
2025-09-02 17:28:12 -0400

    Actually fixes division (I think)

--- project/testing/test_calculator.gd
+++ project/testing/test_calculator.gd
@@ -70,28 +70,24 @@ func test_square_out_of_range():
 	var result: String = calculator_script.square_Quinary_String("45")
 	assert_eq(result, "error")
 
-func test_divide_calc_pass1():
+#Testing divide methods
+func test_divide_normal():
 	var calculator_script = load("res://calculator/calculator.gd").new()
-	var division_result : int = calculator_script.divide_calc(4, 2)
-	assert_eq(division_result, 2, "4 / 2 = 2")	
-
-func test_divide_calc_pass2():
+	var result: String = calculator_script.divide_Quinary_Strings("40", "4")
+	assert_eq(result, "10")
+func test_divide_by_zero():
 	var calculator_script = load("res://calculator/calculator.gd").new()
-	var division_result : int = calculator_script.divide_calc(4, 0)
-	assert_eq(division_result, -100, "4 / 0 = undefined, which is shown as -100 in calculation")
-
-func test_divide_calc_pass3():
+	var result: String = calculator_script.divide_Quinary_Strings("2", "0")
+	assert_eq(result, "error: undefined")
+func test_divide_negative():
 	var calculator_script = load("res://calculator/calculator.gd").new()
-	var division_result : int = calculator_script.divide_calc(5, -1)
-	assert_eq(division_result, -100, "5 / -1 = undefined, which is shown as -100. (Numbers above 4 or 
-		below 0 not allowed), shown as -100 in calculation)")
-
-func test_divide_out_pass1():
+	var result: String = calculator_script.divide_Quinary_Strings("-2", "2")
+	assert_eq(result, "error")
+func test_divide_non_numeric():
 	var calculator_script = load("res://calculator/calculator.gd").new()
-	var output : String = calculator_script.divide_out(-100)
-	assert_eq(output, "Undefined", "An undefined output was given from calculation")
-
-func test_divide_out_pass2():
+	var result: String = calculator_script.divide_Quinary_Strings("a", "2")
+	assert_eq(result, "error")
+func test_divide_out_of_range():
 	var calculator_script = load("res://calculator/calculator.gd").new()
-	var output : String = calculator_script.divide_out(2)
-	assert_eq(output, "2.0", "The output of the calculation was 2.0")
+	var result: String = calculator_script.divide_Quinary_Strings("8", "2")
+	assert_eq(result, "error")
