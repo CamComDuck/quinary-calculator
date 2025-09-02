d4ff7f675ae1d8b1068a6d4d39d34bba50b13ac4
EthanCouch <143107342+EthanCouchBSU@users.noreply.github.com>
2025-09-02 16:24:45 -0400

    added input handling

--- project/calculator/calculator.gd
+++ project/calculator/calculator.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 var last_button_pressed : int = -1
 
+var num1 = ""
+var num2 = ""
+var calcChoice = ""
+var currentNum= 1
+
 @onready var display_text := %DisplayText as DisplayText
 
 #converts the inputed string from the calculaotr into a normal decial to do calulcations on
@@ -51,15 +56,15 @@ func add_Quinary_Strings(input1:String, input2:String) -> String:
 	return base_10_Int_to_Quinary_String(decimal1 + decimal2)
 
 #TODO
-func subtract_Quinary_Strings(input:String) -> String:
+func subtract_Quinary_Strings(input1:String,input2:String) -> String:
 	return""
 	
 #TODO
-func multiply_Quinary_Strings(input:String) -> String:
+func multiply_Quinary_Strings(input1:String,input2:String) -> String:
 	return""
 	
 #TODO
-func divide_Quinary_Strings(input:String) -> String:
+func divide_Quinary_Strings(input1:String,input2:String) -> String:
 	return""
 	
 #TODO
@@ -74,6 +79,8 @@ func square_Quinary_String(input:String) -> String:
 		return "error"
 	return base_10_Int_to_Quinary_String(decimal*decimal)
 
+
+#rework these into the divide_Quinary_Strings method above
 func divide_calc(num_1 : int, num_2 : int) -> float:
 	if num_2 == 0:
 		return -100
@@ -89,10 +96,111 @@ func divide_out(input : float) -> String:
 		return str(input)
 		
 		
-
+func reset_on_calc():
+	num2 = ""
+	calcChoice = ""
+	currentNum = 1
+	
+func full_reset():
+	num1 = ""
+	num2 = ""
+	calcChoice = ""
+	currentNum = 1
+	
 
 func _update_display(new_display_text : String) -> void:
 	display_text.update_display(new_display_text)
 
 func _on_button_pressed(button_pressed : ButtonsGrid.calc_button) -> void:
 	print(ButtonsGrid.calc_button.keys()[button_pressed])
+	
+	#defintely a better way to do this, I just havent looked into it yet
+	
+	
+	#------------------------numbers-------------------------------------
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "ZERO":
+		if currentNum == 1:
+			num1+="0"
+		elif currentNum == 2:
+			num2+="0"
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "ONE":
+		if currentNum == 1:
+			num1+="1"
+		elif currentNum == 2:
+			num2+="1"
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "TWO":
+		if currentNum == 1:
+			num1+="2"
+		elif currentNum == 2:
+			num2+="2"
+	
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "THREE":
+		if currentNum == 1:
+			num1+="3"
+		elif currentNum == 2:
+			num2+="3"
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "FOUR":
+		if currentNum == 1:
+			num1+="4"
+		elif currentNum == 2:
+			num2+="4"
+			
+		#-------------------Methods---------------------------
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "ADD":
+		if calcChoice == "" and num1 != "":
+			calcChoice = "+"
+			currentNum = 2
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "SUBTRACT":
+		if calcChoice == "" and num1 != "":
+			calcChoice = "-"
+			currentNum = 2
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "MULTIPLY":
+		if calcChoice == "" and num1 != "":
+			calcChoice = "*"
+			currentNum = 2
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "DIVIDE":
+		if calcChoice == "" and num1 != "":
+			calcChoice = "/"
+			currentNum = 2
+			
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "SQUARE":
+		if  num1 != "" or num2 != "":
+			if currentNum == 1:
+				num1 = square_Quinary_String(num1)
+			elif currentNum == 2:
+				num2 = square_Quinary_String(num2)
+				
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "ROOT":
+		if  num1 != "" or num2 != "":
+			if currentNum == 1:
+				num1 = squareRT_Quinary_String(num1)
+			elif currentNum == 2:
+				num2 = squareRT_Quinary_String(num2)
+	
+	if ButtonsGrid.calc_button.keys()[button_pressed] == "EQUAL":
+		if  num1 != "" and num2 != "" and calcChoice != "":
+			if calcChoice == "+":
+				num1 = add_Quinary_Strings(num1,num2)
+				reset_on_calc()
+			elif calcChoice == "-":
+				num1 = subtract_Quinary_Strings(num1,num2)
+				reset_on_calc()
+			elif calcChoice == "*":
+				num1 =multiply_Quinary_Strings(num1,num2)
+				reset_on_calc()
+				currentNum
+			elif calcChoice == "/":
+				num1 =divide_Quinary_Strings(num1,num2)
+				reset_on_calc()
+	
+		
+			
+	_update_display(num1 + calcChoice + num2)
+	
