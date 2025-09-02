a3d788d31dc55328cb0bd4591a8a21a714a3bfd9
EthanCouch <143107342+EthanCouchBSU@users.noreply.github.com>
2025-09-02 15:38:27 -0400

    added quinary and decimal converions, tests and addition and square root functions. All tests passed

--- project/calculator/calculator.gd
+++ project/calculator/calculator.gd
@@ -5,6 +5,58 @@ var last_button_pressed : int = -1
 
 @onready var display_text := %DisplayText as DisplayText
 
+#converts the inputed string from the calculaotr into a normal decial to do calulcations on
+func quinary_String_to_Base_10_Int(input:String) -> int:
+	#check for empty string
+	if input == "":
+		return -1
+	#check for characters in the string that are not within the quinary bounds (0-4)
+	for character in input:
+		if character < "0" or character > "4":
+			return -2
+	var output := 0
+	#for each character in the inputted length
+	for i in range(input.length()):
+		#covert the character into an integer, X
+		var x := int(input[i])
+		#convert to base 10 using this formula
+		output = output *5 +x
+	return output
+	
+#converts a decimal back into a quinary string in order to be displayed eventually
+func base_10_Int_to_Quinary_String(input:int) -> String:
+	#dont allow negatives
+	if input <0:
+		return ""
+	#base case
+	if input ==0:
+		return "0"
+	var output := "" 
+	#convert each digit of the input
+	while input >0:
+		# % input by 5 converts the digit into base 5, concat to the output string
+		output =str(input %5) +output
+		# this removes the digit previously calculated thru int division, like dividing something by 10
+		# in base 10 does (150/10 = 15, the 0 is effectively removed)
+		input = input/5
+	return output
+	
+#add two quinary strings together, return a quinary string
+func add_Quinary_Strings(input1:String, input2:String) -> String:
+	var decimal1 := quinary_String_to_Base_10_Int(input1)
+	var decimal2 := quinary_String_to_Base_10_Int(input2)
+	
+	if decimal1 <0 or decimal2 <0:
+		return "error"
+	return base_10_Int_to_Quinary_String(decimal1 + decimal2)
+	
+#square one quinary string
+func square_Quinary_String(input:String) -> String:
+	var decimal := quinary_String_to_Base_10_Int(input)
+	if decimal <0:
+		return "error"
+	return base_10_Int_to_Quinary_String(decimal*decimal)
+
 func divide_calc(num_1 : int, num_2 : int) -> float:
 	if num_2 == 0:
 		return -100
@@ -18,6 +70,9 @@ func divide_out(input : float) -> String:
 		return "Undefined"
 	else:
 		return str(input)
+		
+		
+
 
 func _update_display(new_display_text : String) -> void:
 	display_text.update_display(new_display_text)
--- project/testing/test_calculator.gd
+++ project/testing/test_calculator.gd
@@ -1,6 +1,74 @@
 class_name TestCalculator
 extends GutTest
 
+#test the base 10 to quinary string method
+func test_d2q_negative():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.base_10_Int_to_Quinary_String(-1)
+	assert_eq(result, "")
+func test_d2q_zero():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.base_10_Int_to_Quinary_String(0)
+	assert_eq(result, "0")
+func test_d2q_normal():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.base_10_Int_to_Quinary_String(32)
+	assert_eq(result, "112")
+	
+	
+#test the quinary string back to base 10 method
+func test_q2d_empty_string():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: int = calculator_script.quinary_String_to_Base_10_Int("")
+	assert_eq(result, -1)
+func test_q2d_nonnumeric():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: int = calculator_script.quinary_String_to_Base_10_Int("abc")
+	assert_eq(result, -2)
+func test_q2d_out_of_range():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: int = calculator_script.quinary_String_to_Base_10_Int("156")
+	assert_eq(result, -2)
+func test_q2d_normal():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: int = calculator_script.quinary_String_to_Base_10_Int("112")
+	assert_eq(result, 32)
+
+#test the additon methods
+func test_add_normal():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.add_Quinary_Strings("112","32")
+	assert_eq(result, "144")
+func test_add_negative():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.add_Quinary_Strings("-2","32")
+	assert_eq(result, "error")
+func test_add_non_numeric():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.add_Quinary_Strings("2","32abc")
+	assert_eq(result, "error")
+func test_add_out_of_range():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.add_Quinary_Strings("87","326")
+	assert_eq(result, "error")
+
+#test the squaring
+func test_square_normal():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.square_Quinary_String("43")
+	assert_eq(result, "4104")
+func test_square_negative():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.square_Quinary_String("-2")
+	assert_eq(result, "error")
+func test_square_non_numeric():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.square_Quinary_String("43abc")
+	assert_eq(result, "error")
+func test_square_out_of_range():
+	var calculator_script = load("res://calculator/calculator.gd").new()
+	var result: String = calculator_script.square_Quinary_String("45")
+	assert_eq(result, "error")
 
 func test_divide_calc_pass1():
 	var calculator_script = load("res://calculator/calculator.gd").new()
